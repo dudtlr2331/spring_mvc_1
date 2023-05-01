93fbde6e64d334d7c44fe7c43c34ea370fff8132
Cat Hanbit <dudtlr102@naver.com>
2023-05-01 15:28:28 +0900

    요청 매핑 - API 예시 완료

--- item-service/item-service/src/main/java/hello/itemservice/basic/requestmapping/MappingClassController.java
+++ item-service/item-service/src/main/java/hello/itemservice/basic/requestmapping/MappingClassController.java
@@ -4,6 +4,15 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/mapping/users")
 public class MappingClassController {
+   /**
+    * 회원 관리 API
+    * 회원 목록 조회: GET /users
+    * 회원 등록: POST /users
+    * 회원 조회: GET /users/{userId}
+    * 회원 수정: PATCH /users/{userId}
+    * 회원 삭제: DELETE /users/{userId}
+    */
+
     /**
      * GET /mapping/users
      */
