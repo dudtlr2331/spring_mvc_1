0de4d4d8fe64bb20bef3f463cc97a58a0ec2d819
Cat Hanbit <dudtlr102@naver.com>
2023-05-01 16:09:24 +0900

    요청 파라미터 - 쿼리 파라미터, HTML Form 완료

--- item-service/item-service/src/main/java/hello/itemservice/basic/request/RequestParamController.java
+++ item-service/item-service/src/main/java/hello/itemservice/basic/request/RequestParamController.java
@@ -1,6 +1,6 @@
 package hello.itemservice.basic.request;
 
-import hello.itemservice.basic.HelloData;
+//import hello.itemservice.basic.HelloData;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -16,11 +16,11 @@ public class RequestParamController {
      * 반환 타입이 없으면서 이렇게 응답에 값을 직접 집어넣으면, view 조회X
      */
     @RequestMapping("/request-param-v1")
-    public void requestParamV1(HttpServletRequest request, HttpServletResponse
-            response) throws IOException {
+    public void requestParamV1(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String username = request.getParameter("username");
         int age = Integer.parseInt(request.getParameter("age"));
         log.info("username={}, age={}", username, age);
+
         response.getWriter().write("ok");
     }
 }
